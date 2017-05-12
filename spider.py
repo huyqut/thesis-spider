1307ca4e9a4dd82f4963e4dd8d0d0166b5b3fab5
huyqut <huy.qut@gmail.com>
2017-05-12 20:23:09 +0700

    Integrate saving links to mongdb

--- spider.py
+++ spider.py
@@ -1,6 +1,7 @@
 from twitter_dev import TwitterDev
 import re
-import geograpy
+import pymongo
+import time
 
 
 class Spider:
@@ -9,18 +10,23 @@ class Spider:
     def crawl_feeds(dev: TwitterDev = None):
         if dev is None:
             return
+        mongo_client = pymongo.MongoClient('localhost', 27017)
+        mongo_database = mongo_client['news-database']
+        news_collection = mongo_database['news-collection']
         user_id = dev.api.VerifyCredentials().AsDict()['id']
         friends = dev.api.GetFriendIDs(user_id, stringify_ids = True)
         for status in dev.api.GetStreamFilter(follow = friends):
-            if not status['truncated']:
-                message = status['text']
-            else:
-                message = status['extended_tweet']['full_text']
+            message = status['text']
             url_match = re.search("(?P<url>https?://[^\s]+)", message)
             if url_match is None:
                 continue
             url = url_match.group(0)
             if len(url) < 23:
                 continue
-            print(url)
-            print(geograpy.get_place_context(url).places)
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(status['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+            document = {
+                'id': status['id'],
+                'created_at': timestamp,
+                'reference': url
+            }
+            news_collection.insert_one(document)
